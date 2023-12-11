c0aa3db1612f714c9dc4d0c60c46a3535af8f12d
thibautRV <125503955+thibautRV@users.noreply.github.com>
2023-12-11 11:00:45 +0100

    kafka fonctionnel

--- kafka/producer/ticker_generator.py
+++ kafka/producer/ticker_generator.py
@@ -1,31 +1,6 @@
-from flask import Flask, request, jsonify
-from confluent_kafka import Producer
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers='kafka:9092')
 
-app = Flask(__name__)
+producer.send('foobar', b'some_message_bytes')
 
-# Kafka broker address
-bootstrap_servers = 'localhost:9092'
-
-# Kafka topic to produce tickets
-ticket_topic = 'ticket_topic'
-
-# Create Kafka producer configuration
-producer_config = {
-    'bootstrap.servers': bootstrap_servers,
-}
-
-# Create Kafka producer instance
-producer = Producer(producer_config)
-
-@app.route('/', methods=['POST'])
-def receive_ticket():
-    ticket_data = request.data.decode('utf-8')
-
-    # Produce the ticket to Kafka topic
-    producer.produce(ticket_topic, value=ticket_data)
-    producer.flush()
-
-    return 'Ticket received and produced to Kafka\n'
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+producer.flush()
