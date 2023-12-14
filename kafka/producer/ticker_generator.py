9d5a18bff3be6ce65d255bc73c86c7b9c0953c7b
thibautRV <125503955+thibautRV@users.noreply.github.com>
2023-12-14 23:07:35 +0100

    ff

--- kafka/producer/ticker_generator.py
+++ kafka/producer/ticker_generator.py
@@ -1,8 +1,8 @@
+from random import randint, sample, choice
 from kafka import KafkaProducer
 import json
 import schedule
 import time
-from random import choice
 
 bootstrap_servers = 'kafka:9092'
 topic = 'tickets'
@@ -13,13 +13,11 @@ store_id = 1  # Replace with the actual store ID
 
 def generate_ticket():
     # Simulate ticket generation
-    ticket_data = {
-        "store_id": store_id,
-        "product": choice(["Apple", "Orange", "Bottle of water"]),
-        "quantity": 1,
-        "price": 0  # You can calculate the total price based on product and quantity
-    }
-
+    store_id = randint(1, 100)
+    checkout_id = randint(1, 1000)
+    seller_id = randint(1, 1200)
+    basket = {product_name: randint(1, 5) for product_name in sample(["Apple", "Orange", "Milk", "Bottle of water"], randint(1, 4))}    
+    ticket_data = {"store_id": store_id, "checkout_id": checkout_id, "seller_id": seller_id, "basket": basket, "total_price": sum(product_prices[product_name]*quantity for product_name, quantity in basket.items())}
     producer.send(topic, value=ticket_data)
     print(f'Ticket produced: {ticket_data}')
 
